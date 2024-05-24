2daef6fb006e95b0370533433b11cf2e6cbb1c5d
Justin Beeson <justinbeeson@gmail.com>
2024-05-24 09:52:32 -0700

    use a preview version to debug

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 ﻿$BuildPSModule = @{
     Name        = 'AzDOCmd'
-    Version     = '0.0.5'
+    Version     = '0.0.5-preview.1'
     Description = 'A module for interacting with Azure DevOps.'
     Tags        = ('PSEdition_Desktop', 'PSEdition_Core', 'Azure', 'AzureDevOps', 'AzurePipelines')
 }
