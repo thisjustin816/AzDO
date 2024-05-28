5a4c053ea26dd9dca9526e840ceb38f94184aab0
Justin Beeson <justinbeeson@gmail.com>
2024-05-28 11:32:21 -0700

    feat(pipelines): :sparkles: Add Get-AzDOPipelineRun and Get-AzDOPipelineRunList
    Add functions to get pipeline runs.

---
+++ src/public/pipelines/Get-AzDOPipelineRun.ps1
@@ -0,0 +1,223 @@
+﻿<#
+.SYNOPSIS
+Gets details for a specific build by ID or user email.
+
+.DESCRIPTION
+Gets details for a specific build by ID or user email, with filters for build result, status, and reason.
+
+.PARAMETER BuildId
+The ID of the build to get.
+
+.PARAMETER User
+The email of the user that the build(s) to get was requested for.
+
+.PARAMETER Result
+Only return builds with the specified result.
+
+.PARAMETER Status
+Only return builds with the specified status.
+
+.PARAMETER Reason
+Only return builds with the specified reason.
+
+.PARAMETER MaxBuilds
+The maximum number of builds per project to get. Defaults to 10.
+
+.PARAMETER Project
+Project that the build's pipeline resides in.
+
+.PARAMETER Pat
+Personal access token authorized to administer builds. Defaults to $env:SYSTEM_ACCESSTOKEN for use in Azure
+Pipelines.
+
+.EXAMPLE
+Get-AzDOPipelineRun -BuildId 111111 -Project MyProject
+Gets the build with the specified ID.
+
+.EXAMPLE
+Get-AzDOPipelineRun -User myorg@dev.azure.com -Status completed -Reason buildCompletion
+Gets 10 completed builds that were triggered by another build's completion started by myorg@dev.azure.com.
+
+.NOTES
+The Cmdlet will work as-is in a UI Pipeline with the default $Pat parameter as long as OAUTH access has been
+enabled for the pipeline/job. If using a YAML build, the system.accesstoken variable needs to be explicitly
+mapped to the steps environment like the following example:
+
+steps:
+- powershell: Invoke-WebRequest -Uri $Uri -Headers ( Initialize-AzRestApi )
+  env:
+    SYSTEM_ACCESSTOKEN: $(system.accesstoken)
+.LINK
+https://docs.microsoft.com/en-us/rest/api/azure/devops/build/builds/get?view=azure-devops-rest-6.0
+#>
+
+function Get-AzDOPipelineRun {
+    [CmdletBinding(DefaultParameterSetName = 'BuildId')]
+    param (
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            Mandatory = $true,
+            Position = 1,
+            ParameterSetName = 'BuildId'
+        )]
+        [Alias('id')]
+        [Int[]]
+        $BuildId,
+
+        [Parameter(
+            Mandatory = $true,
+            ParameterSetName = 'User'
+        )]
+        [Alias('requestedFor')]
+        [String[]]
+        $User,
+
+        [Parameter(
+            ParameterSetName = 'User'
+        )]
+        [ValidateSet(
+            'canceled',
+            'failed',
+            'none',
+            'partiallySucceeded',
+            'succeeded'
+        )]
+        [String[]]
+        $Result,
+
+        [Parameter(
+            ParameterSetName = 'User'
+        )]
+        [ValidateSet(
+            'all',
+            'cancelling',
+            'completed',
+            'inProgress',
+            'none',
+            'notStarted',
+            'postponed'
+        )]
+        [String[]]
+        $Status,
+
+        [Parameter(
+            ParameterSetName = 'User'
+        )]
+        [ValidateSet(
+            'all',
+            'batchedCI',
+            'buildCompletion',
+            'checkInShelveset',
+            'individualCI',
+            'manual',
+            'none',
+            'pullRequest',
+            'resourceTrigger',
+            'schedule',
+            'scheduleForced',
+            'triggered',
+            'userCreated',
+            'validateShelveset'
+        )]
+        [String[]]
+        $Reason,
+
+        [Parameter(
+            ParameterSetName = 'User'
+        )]
+        [String]
+        $MaxBuilds = 10,
+
+        [Switch]
+        $NoRetry,
+
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [System.Object[]]
+        $Project = $env:SYSTEM_TEAMPROJECT,
+
+        [String]
+        $CollectionUri = $env:SYSTEM_COLLECTIONURI,
+
+        [string]
+        $Pat = $env:SYSTEM_ACCESSTOKEN
+    )
+
+    begin {
+        $script:AzApiHeaders = @{
+            Headers       = Initialize-AzRestApi -Pat $Pat
+            CollectionUri = $CollectionUri
+            ApiVersion    = '6.1'
+        }
+    }
+
+    process {
+        . $PSScriptRoot/..//private/Get-AzApiProjectName.ps1
+        $Project = $Project | Get-AzApiProjectName
+
+        foreach ($id in $BuildId) {
+            foreach ($projectName in $Project) {
+                try {
+                    $buildInfo = Invoke-AzDORestApiMethod `
+                        @script:AzApiHeaders `
+                        -Method Get `
+                        -Project $projectName `
+                        -Endpoint "build/builds/$id" `
+                        -NoRetry `
+                        -WhatIf:$false `
+                        -ErrorAction SilentlyContinue
+                }
+                catch {
+                    Write-Verbose -Message (
+                        $_ | Out-String
+                    )
+                }
+                if ($buildInfo) {
+                    $buildInfo
+                }
+                else {
+                    Write-Warning -Message "Build $id not found in $projectName."
+                }
+            }
+        }
+
+        foreach ($email in $User) {
+            $params = @(
+                "requestedFor=$email",
+                "`$top=$MaxBuilds",
+                'buildQueryOrder=queueTimeDescending'
+            )
+            if ($Result) {
+                $params += "resultFilter=$($Result -join ',')"
+            }
+            if ($Status) {
+                $params += "statusFilter=$($Status -join ',')"
+            }
+            if ($Reason) {
+                $params += "reasonFilter=$($Reason -join ',')"
+            }
+
+            foreach ($projectName in $Project) {
+                $buildInfo = Invoke-AzDORestApiMethod `
+                    @script:AzApiHeaders `
+                    -Method Get `
+                    -Project $projectName `
+                    -Endpoint 'build/builds' `
+                    -Params $params `
+                    -NoRetry:$NoRetry `
+                    -WhatIf:$false
+
+                if ($buildInfo) {
+                    $buildInfo
+                }
+                else {
+                    Write-Warning -Message (
+                        "$($Result -join '/')/$($Status -join '/')/$($Reason -join '/') " +
+                        "builds for $User not found in $projectName."
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ src/public/pipelines/Get-AzDOPipelineRunList.Tests.ps1
@@ -0,0 +1,81 @@
+﻿Describe 'Integration Tests' -Tag 'Integration' {
+    BeforeAll {
+        Get-Module -Name AzDOCmd -All | Remove-Module -Force -ErrorAction SilentlyContinue
+        Import-Module -Name "$PSScriptRoot/../../AzDOCmd.psm1" -Force
+    }
+
+    It 'should return builds in order of id' {
+        $builds = Get-BuildPipeline -Name PipelineTest -Project Tools |
+            Get-AzDOPipelineRunList -MaxBuilds 10 -IncludePr
+        $builds.id | Should -BeExactly ( $builds.id | Sort-Object -Descending )
+    }
+
+    It 'should exclude PR builds if specified' {
+        $fiftyBuilds = Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -MaxBuilds 10
+        ( $fiftyBuilds | Select-Object -ExpandProperty reason ) -join ',' |
+            Should -Not -Match 'pullRequest'
+        $fiftyBuilds.Count | Should -BeLessOrEqual 10
+    }
+
+    It 'should still get a build if MaxBuilds is 1 and most recent is a PR' {
+        $nonPrBuild = Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -MaxBuilds 1
+        $nonPrBuild | Should -HaveCount 1
+        $nonPrBuild.reason | Should -Not -Be 'pullRequest'
+    }
+
+    It 'should only get in progress builds if specified' {
+        Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -InProgress -IncludePr |
+            ForEach-Object -Process {
+                ('inProgress', 'notStarted') | Should -Contain $_.status
+            }
+    }
+
+    It 'should only get completed builds if specified' {
+        Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -MaxBuilds 25 -HasResult -IncludePr |
+            ForEach-Object -Process {
+                $_.status | Should -Match 'completed'
+            }
+    }
+
+    It 'should only get succeeded builds if specified' {
+        Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -MaxBuilds 25 -Succeeded -IncludePr |
+            ForEach-Object -Process {
+                $_.result | Should -Match 'succeeded'
+            }
+    }
+
+    It 'should only return builds in the past <_> days' -TestCases @(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21) {
+        $historyInDays = $_
+        $threshold = [DateTime]::Today.AddDays(-$historyInDays).ToUniversalTime()
+        Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -HistoryInDays $historyInDays |
+            ForEach-Object -Process {
+                if (( $_.finishTime | Get-Date ).ToUniversalTime() -lt $threshold) {
+                    Write-Host -Object "Build outside threshold: $($_.id)"
+                }
+                ( $_.finishTime | Get-Date ).ToUniversalTime() | Should -BeGreaterThan $threshold
+            }
+    }
+
+    It 'should give a warning if no builds were found' {
+        Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -Branch non-existent-branch |
+            Should -HaveCount 0
+        Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -Branch non-existent-branch 3>&1 |
+            Should -Match 'No builds found'
+    }
+
+    It 'should only return builds from a given branch' {
+        Get-BuildPipeline -Name AzDOCmd -Project Tools |
+            Get-AzDOPipelineRunList -MaxBuilds 50 -IncludePr -Branch integration |
+            ForEach-Object -Process {
+                $_.sourceBranch | Should -BeExactly 'refs/heads/integration'
+            }
+    }
+}
