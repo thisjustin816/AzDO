9c14fe91d1eb91908704b8fcbd61399532cd01d0
Justin Beeson <justinbeeson@gmail.com>
2024-05-29 07:03:12 -0700

    feat(Get-AzDOWorkItem): :sparkles: Add ability to search for a work item by title

--- src/public/work/Get-AzDOWorkItem.ps1
+++ src/public/work/Get-AzDOWorkItem.ps1
@@ -23,14 +23,19 @@ Get-AzDOWorkItem -Id 12345 -Project MyProject
 .LINK
 https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/work-items/get-work-item
 
+.LINK
+https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/wiql/query-by-wiql
+
 .NOTES
 N/A
 #>
 function Get-AzDOWorkItem {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'ID')]
     param (
-        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [Parameter(ParameterSetName = 'ID', Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
         [String[]]$Id,
+        [Parameter(ParameterSetName = 'Title', Mandatory = $true, Position = 0)]
+        [String]$Title,
         [Switch]$NoRetry,
         [String]$Project = $env:SYSTEM_TEAMPROJECT,
         [String]$CollectionUri = $env:SYSTEM_COLLECTIONURI,
@@ -41,11 +46,32 @@ function Get-AzDOWorkItem {
         $script:AzApiHeaders = @{
             Headers       = Initialize-AzDORestApi -Pat $Pat
             CollectionUri = $CollectionUri
-            ApiVersion    = '7.1-preview.3'
+            ApiVersion    = '7.1-preview.2'
         }
     }
 
     process {
+        if ($Title) {
+            $body = @{
+                query = "SELECT [System.Id] FROM workitems WHERE [System.Title] CONTAINS '$Title'"
+            } | ConvertTo-Json -Compress
+    
+            $Id = @(
+                Invoke-AzDORestApiMethod `
+                    @script:AzApiHeaders `
+                    -Method Post `
+                    -Project $Project `
+                    -Endpoint "wit/wiql" `
+                    -Body $body `
+                    -NoRetry:$NoRetry |
+                    Select-Object -ExpandProperty workItems |
+                    Select-Object -ExpandProperty id
+            )
+            if (!$Id) {
+                Write-Warning -Message "No work items found with title $Title in project $Project."
+            }
+        }
+
         foreach ($item in $Id) {
             $workItem = Invoke-AzDORestApiMethod `
                 @script:AzApiHeaders `
