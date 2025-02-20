9501a0c80c384c779a3b68fb3f586e49ff359c2c
Justin Beeson <justinbeeson@gmail.com>
2025-02-20 12:54:49 -0800

    Add Set-AzDODashboard function to create or update Azure DevOps dashboards

---
+++ src/public/info/Set-AzDODashboard.ps1
@@ -0,0 +1,72 @@
+<#
+.SYNOPSIS
+Creates or updates a dashboard in Azure DevOps.
+
+.DESCRIPTION
+Creates or updates a dashboard in Azure DevOps using the dashboard object from Get-AzDODashboard.
+
+.PARAMETER Dashboard
+The dashboard object to create or update.
+
+.PARAMETER Project
+Project that the dashboard resides in.
+
+.PARAMETER CollectionUri
+The project collection URL (https://dev.azure.com/[organization]).
+
+.PARAMETER Pat
+Personal access token authorized to administer dashboards. Defaults to $env:SYSTEM_ACCESSTOKEN for use in Azure
+Pipelines.
+
+.EXAMPLE
+$dashboard = Get-AzDODashboard -Project MyProject -Name MyDashboard
+Set-AzDODashboard -Dashboard $dashboard -Project MyProject
+
+.LINK
+https://learn.microsoft.com/en-us/rest/api/azure/devops/dashboard/dashboards/create
+https://learn.microsoft.com/en-us/rest/api/azure/devops/dashboard/dashboards/update
+
+.NOTES
+M/A
+#>
+
+function Set-AzDODashboard {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [Object]$Dashboard,
+        [Switch]$NoRetry,
+        [String]$Project = $env:SYSTEM_TEAMPROJECT,
+        [String]$CollectionUri = $env:SYSTEM_COLLECTIONURI,
+        [String]$Pat = $env:SYSTEM_ACCESSTOKEN
+    )
+
+    begin {
+        $script:AzApiHeaders = @{
+            Headers       = Initialize-AzDORestApi -Pat $Pat
+            CollectionUri = $CollectionUri
+            ApiVersion    = '7.1-preview.3'
+        }
+    }
+
+    process {
+        $endpoint = if ($Dashboard.id) {
+            Write-Host "Updating dashboard with ID: $($Dashboard.id) in project: $Project"
+            "dashboard/dashboards/$($Dashboard.id)"
+        }
+        else {
+            Write-Host "Creating new dashboard in project: $Project"
+            "dashboard/dashboards"
+        }
+
+        if ($PSCmdlet.ShouldProcess("Dashboard $($Dashboard.id)", "Set")) {
+            Invoke-AzDORestApiMethod `
+                @script:AzApiHeaders `
+                -Method Post `
+                -Project $Project `
+                -Endpoint $endpoint `
+                -Body ( $Dashboard | ConvertTo-Json -Depth 10 ) `
+                -NoRetry:$NoRetry
+        }
+    }
+}
