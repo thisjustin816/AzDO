5fd61ff20cf60b001eecdca9d00e5b9bc23a4a47
Justin Beeson <justinbeeson@gmail.com>
2026-01-07 12:40:31 -0800

    rev version

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $BuildPSModule = @{
     Name        = 'AzDOCmd'
-    Version     = '2.3.0-preview3'
+    Version     = '2.3.0-preview4'
     Description = 'A module for interacting with Azure DevOps.'
     Tags        = (
         'PSEdition_Desktop',
